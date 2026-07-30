58da451899516e6c9b8f93ae2f6d6d20fe27889e
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-07-31 02:30:50 +0530

    hub done1

---
+++ .github/workflows/backend.tf
@@ -0,0 +1,10 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "dhoni-demo-terraform-bucket-123456"
+    key            = "vpc/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}      
